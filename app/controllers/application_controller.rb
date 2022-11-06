71caba32c0723bef2b5418c58503e76d1d3e1ef1
thadeus <thadeusnyariki94@gmail.com>
2022-11-06 18:41:37 +0300

    Getting One Game Using Params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,25 @@
 class ApplicationController < Sinatra::Base
 
-  get '/' do
-    { message: "Hello world" }.to_json
+  # Add this line to set the Content-Type header for all responses
+  set :default_content_type, 'application/json'
+
+  get '/games' do
+    # get all the games from the database
+    games = Game.all
+    # return a JSON response with an array of all the game data
+    games.to_json
+  end
+
+  #sort the games by title instead of the default sort order
+  get '/games' do
+    games = Game.all.order(:title)
+    games.to_json
+  end
+
+  #Or just return the first 10 games:
+  get '/games' do
+    games = Game.all.order(:title).limit(10)
+    games.to_json
   end
 
 end
